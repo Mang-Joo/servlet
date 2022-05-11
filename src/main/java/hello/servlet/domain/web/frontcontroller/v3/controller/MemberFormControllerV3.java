6b00c1facb4c98cc405bf071ac651c654c20801b
KimWanJoo <wjwan0915@gmail.com>
2022-05-11 22:14:09 +0900

    feat: 프론트 컨트롤러 V3 적용
    컨트롤러에 request, response를 제거하고 Map으로 파라미터를 처리 한다. view도 논리적 view 이름만 return하여 처리한다.

--- src/main/java/hello/servlet/domain/web/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ src/main/java/hello/servlet/domain/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -1,4 +1,13 @@
 package hello.servlet.domain.web.frontcontroller.v3.controller;
 
-public class MemberFormControllerV3 {
+import hello.servlet.domain.web.frontcontroller.ModelView;
+import hello.servlet.domain.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form");
+    }
 }
