e078314e63eddb17dc3f5626d63d4e87ec0ad5a3
KimWanJoo <wjwan0915@gmail.com>
2022-05-11 21:39:36 +0900

    feat: 프론트 컨트롤러 V2 적용
    View로 리턴해주는 부분을 따로 빼서 처리

---
+++ src/main/java/hello/servlet/domain/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,29 @@
+package hello.servlet.domain.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.domain.web.frontcontroller.MyView;
+import hello.servlet.domain.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        Member saveMember = memberRepository.save(member);
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", saveMember);
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
+}
---
+++ src/main/java/hello/servlet/domain/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -0,0 +1,4 @@
+package hello.servlet.domain.web.frontcontroller.v3.controller;
+
+public class MemberFormControllerV3 {
+}
